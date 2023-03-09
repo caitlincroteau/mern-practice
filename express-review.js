0904e26b72fde87c82c82085cb79ebb8894b0906
Caitlin Croteau <cjcroteau@gmail.com>
2023-03-09 15:04:29 -0800

    add middleware

--- express-review.js
+++ express-review.js
@@ -1,11 +1,25 @@
+//requirements
 const express = require("express");
+const morgan = require("morgan");
+
+//setup
 const app = express();
 const port = 3000;
+
+//middleware
+app.use(morgan('dev'));
+app.use(express.json());
+
+//routes/endpoints
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+//CRUD operations
+
+
+//listener
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
